3bd09128cfa7299179da40cdf71d8293d01e875f
Intelicopter <intelicopter@gmail.com>
2017-08-27 01:23:38 +0800

    edit

--- playundercover/views.py
+++ playundercover/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.shortcuts import redirect
+from django.contrib.auth import login as auth_login
+from django.core.context_processors import csrf
 
 def home(request):
     return render(request, 'index.html', {})
 
 def login(request):
     return render(request, 'login.html', {})
+
+def authentication(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+
+    def errorHandle(error):
+        c = {}
+        c.update(csrf(request))
+        return render(request, 'login.html', {'error': error})
+
+    if user is not None:
+
+        if user.is_active:
+            auth_login(request, user)
+            c = {}
+            c.update(csrf(request))
+            return redirect('dashboard')
+        else:
+            c = {}
+            c.update(csrf(request))
+            return render(request, "login.html", {})
+    elif user is None:
+        c = {}
+        c.update(csrf(request))
+        error = 'Invalid username/password'
+        return errorHandle(error)
